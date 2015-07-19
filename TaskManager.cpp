7166714688a7f19a88b11972cdbb514a4a5b77aa
Aziz Bekkine <abekkine@gmail.com>
2015-07-20 00:41:39 +0300

    TaskManager updates
    - TaskManager class populated with required public (but stub) methods.
    - Methods required to add -just created- models and interfaces included (as stubs).
    - Proper TODO statements included as comments.

--- TaskManager.cpp
+++ TaskManager.cpp
@@ -3,7 +3,29 @@
 #include "TaskManager.h"
 
 TaskManager::TaskManager() {
+    // TODO(abekkine) : CTOR
 }
 
 TaskManager::~TaskManager() {
+    // TODO(abekkine) : DTOR
+}
+
+void TaskManager::AddModel(Model* modelInstance, double freqInHz) {
+    // TODO(abekkine) : Add a model instance.
+    modelInstance = modelInstance;
+    freqInHz = freqInHz;
+}
+
+void TaskManager::AddInterface(Interface* interfaceInstance, double freqInHz) {
+    // TODO(abekkine) : Add an interface instance.
+    interfaceInstance = interfaceInstance;
+    freqInHz = freqInHz;
+}
+
+void TaskManager::Initialize() {
+    // TODO(abekkine) : Initialize task manager.
+}
+
+void TaskManager::Execute() {
+    // TODO(abekkine) : Start scheduling.
 }
--- TaskManager.h
+++ TaskManager.h
@@ -1,11 +1,19 @@
 // Copyright 2015 A.Bekkine
+
 #ifndef TASKMANAGER_H_
 #define TASKMANAGER_H_
+
+#include "Model.h"
+#include "Interface.h"
 
 class TaskManager {
  public:
     TaskManager();
     ~TaskManager();
+    void AddModel(Model* modelInstance, double freqInHz);
+    void AddInterface(Interface* interfaceInstance, double freqInHz);
+    void Initialize();
+    void Execute();
 };
 
 #endif  // TASKMANAGER_H_
