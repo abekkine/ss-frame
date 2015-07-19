2c57f891ee46d279ca8b7bb91e663f8f30ab281e
Aziz Bekkine <abekkine@gmail.com>
2015-07-20 00:40:54 +0300

    New classes
    - Model and Interface dummy classes added to project.

---
+++ Interface.h
@@ -0,0 +1,12 @@
+// Copyright 2015 A.Bekkine
+
+#ifndef INTERFACE_H_
+#define INTERFACE_H_
+
+class Interface {
+ public:
+    Interface() {}
+    ~Interface() {}
+};
+
+#endif  // INTERFACE_H_
---
+++ Model.h
@@ -0,0 +1,12 @@
+// Copyright 2015 A.Bekkine
+
+#ifndef MODEL_H_
+#define MODEL_H_
+
+class Model {
+ public:
+    Model() {}
+    ~Model() {}
+};
+
+#endif  // MODEL_H_
