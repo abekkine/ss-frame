6c4ce66556c5d3c9c2d6aa091b8a89dcc9dc7584
A.Bekkine <abekkine@gmail.com>
2015-07-19 23:05:19 +0300

    Initial commit:
    - Main application created with what to do (as comments).
    - Ignore file updated with new binary (.bin).

---
+++ main.cpp
@@ -0,0 +1,11 @@
+int main() {
+
+    // [1] Initialize our task manager.
+    // [2] Add models with frequencies to task manager.
+    // [3] Add interfaces with frequencies to task manager.
+    // [4] Initialize everything.
+    // [5] Start running.
+    // [6] When (if) done, clean up and exit.
+
+    return 0;
+}
